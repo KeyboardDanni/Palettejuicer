8ef1189a35db2090d86dfb8271db6eac18192dc2
Danni <34800072+KeyboardDanni@users.noreply.github.com>
2024-03-28 18:37:07 -0400

    Initial commit, basic project and style

---
+++ README.md
@@ -0,0 +1,10 @@
+# Palettejuicer: An easy way to create color palettes for pixel art
+
+## Building and Running
+
+- Install npm if necessary, then run `npm install` within the base directory.
+- Start the test server using `npm run dev`.
+
+## License
+
+This software is provided under the ISC License. See `LICENSE` for more information.
---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div id="logo" />
+      <div className="section">
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <h4>Heading 4</h4>
+        <h5>Heading 5</h5>
+        <h6>Heading 6</h6>
+      </div>
+      <div className="section">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
