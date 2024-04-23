9a5507e0fde93b3962eb0e8c1b233cd2738a4924
Danni <34800072+KeyboardDanni@users.noreply.github.com>
2024-04-23 17:24:47 -0400

    Reenable immer autofreeze

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import "reflect-metadata";
 
-import { setAutoFreeze } from "immer";
-
-// Needed so that class-transformer works with immer
-setAutoFreeze(false);
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
